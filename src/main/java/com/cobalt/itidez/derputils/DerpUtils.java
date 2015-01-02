f095fa5f5f8cd2e533fa65aea05a94bca4f9e482
iTidez <iTidez@gmail.com>
2015-01-01 17:55:40 -0800

    Init Commit

---
+++ src/main/java/com/cobalt/itidez/derputils/DerpUtils.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cobalt.itidez.derputils;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author iTidez
+ */
+public class DerpUtils extends JavaPlugin {
+    
+    @Override
+    public void onEnable() {
+        
+    }
+    
+    @Override
+    public void onDisable() {
+        
+    }
+    
+    public DerpUtils() {
+        
+    }
+}
